a4fd94c03a7908203cb6fdf3ca985866626ea39a
Amit <amitrogel97@gmail.com>
2023-04-17 12:13:27 -0400

    Simplfied xarm communication, removed noise filtering

--- Demos/VisionTrackerDemo.py
+++ Demos/VisionTrackerDemo.py
@@ -1,6 +1,5 @@
 from Demos.Interfaces.IRobotDemo import IRobotDemo
 from Handlers.VisionHandler import VisionHandler
-from Helpers.DataFilters import buffered_smooth, save_joint_data, save_vision_data
 
 import time
 from threading import Thread
@@ -21,13 +20,6 @@ class VisionTrackerDemo(IRobotDemo):
         self.vision_thread = Thread(target=self.vision.start)
         self.listener_thread = Thread(target=self._listener)
 
-        self.head_x = []
-        self.head_y = []
-        self.head_z = []
-        self.shoulder_x = []
-        self.shoulder_y = []
-        self.shoulder_z = []
-
     def start(self):
         self.announceStart()
         self.readyRobots()
@@ -40,54 +32,47 @@ class VisionTrackerDemo(IRobotDemo):
     def _listener(self):
         print("Listening queue started")
         while True:
-            address, gesture = self.communication_queue.get()
+            # print(self.communication_queue)
+            address, data = self.communication_queue.get()
 
             if address == "/gesture":
                 mode = "pose"
-                print(gesture)
-                if gesture == "stop":
+                # print(data)
+                if data == "stop":
                     self.qList[0].put([mode, 1])
                     self.qList[1].put([mode, 1])
                     self.qList[2].put([mode, 1])
                     self.qList[3].put([mode, 1])
                     self.qList[4].put([mode, 1])
-                elif gesture == "go":
+                elif data == "go":
                     self.qList[0].put([mode, 2])
                     self.qList[1].put([mode, 2])
                     self.qList[2].put([mode, 2])
                     self.qList[3].put([mode, 2])
                     self.qList[4].put([mode, 2])
-                elif gesture == "swipe_left":
+                elif data == "swipe_left":
                     self.qList[0].put([mode, 3])
                     self.qList[1].put([mode, 3])
                     self.qList[2].put([mode, 3])
                     self.qList[3].put([mode, 3])
                     self.qList[4].put([mode, 3])
-                elif gesture == "swipe_right":
+                elif data == "swipe_right":
                     self.qList[0].put([mode, 4])
                     self.qList[1].put([mode, 4])
                     self.qList[2].put([mode, 4])
                     self.qList[3].put([mode, 4])
                     self.qList[4].put([mode, 4])
-                elif gesture == "twirl":
+                elif data == "twirl":
                     self.qList[0].put([mode, 5])
                     self.qList[1].put([mode, 5])
                     self.qList[2].put([mode, 5])
                     self.qList[3].put([mode, 5])
                     self.qList[4].put([mode, 5])
 
-            # TODO: replace args[0], args[1] .... from communication queue
-            elif address == "/head":
-                raise NotImplementedError
-                head = {'x': args[0], 'y': args[1], 'z': args[2]}
-                shoulder = {'x': args[3], 'y': args[4], 'z': args[5]}
-
-                smoothed_head = buffered_smooth(self.head_x, self.head_y, self.head_z, head)
-                smoothed_shoulder = buffered_smooth(self.shoulder_x, self.shoulder_y, self.shoulder_z, shoulder)
+            elif address == "/live":
                 mode = "live"
-
-                if smoothed_head is not None and smoothed_shoulder is not None:
-                    # timestamp = time.time()
-                    # save_vision_data('vision_data.csv', timestamp, [head, shoulder], [smoothed_head, smoothed_shoulder])
-                    data = [smoothed_head, smoothed_shoulder]
-                    # self.qList[0].put([mode, data])
+                self.qList[0].put([mode, data])
+                self.qList[1].put([mode, data])
+                self.qList[2].put([mode, data])
+                self.qList[3].put([mode, data])
+                self.qList[4].put([mode, data])
