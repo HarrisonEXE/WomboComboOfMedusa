a0327f6023eae492f59321ddd7882caee5c65aab
Amit <amitrogel97@gmail.com>
2023-04-19 21:24:53 -0400

    Added drum gesture calls

--- Demos/VisionTrackerDemo.py
+++ Demos/VisionTrackerDemo.py
@@ -16,7 +16,7 @@ class VisionTrackerDemo(IRobotDemo):
 
         self.communication_queue = Queue()
         # self.vision = VisionHandler(device=0, communication_queue=self.communication_queue)
-        self.vision = VisionHandler(device=6, communication_queue=self.communication_queue)
+        self.vision = VisionHandler(device=4, communication_queue=self.communication_queue)
         self.vision_thread = Thread(target=self.vision.start)
         self.listener_thread = Thread(target=self._listener)
 
@@ -37,7 +37,7 @@ class VisionTrackerDemo(IRobotDemo):
 
             if address == "/gesture":
                 mode = "pose"
-                # print(data)
+                print(data)
                 if data == "stop":
                     self.qList[0].put([mode, 1])
                     self.qList[1].put([mode, 1])
@@ -50,25 +50,28 @@ class VisionTrackerDemo(IRobotDemo):
                     self.qList[2].put([mode, 2])
                     self.qList[3].put([mode, 2])
                     self.qList[4].put([mode, 2])
-                elif data == "swipe_left":
-                    self.qList[0].put([mode, 3])
-                    self.qList[1].put([mode, 3])
-                    self.qList[2].put([mode, 3])
-                    self.qList[3].put([mode, 3])
-                    self.qList[4].put([mode, 3])
-                elif data == "swipe_right":
-                    self.qList[0].put([mode, 4])
-                    self.qList[1].put([mode, 4])
-                    self.qList[2].put([mode, 4])
-                    self.qList[3].put([mode, 4])
-                    self.qList[4].put([mode, 4])
+                # elif data == "swipe_left":
+                #     self.qList[0].put([mode, 3])
+                #     self.qList[1].put([mode, 3])
+                #     self.qList[2].put([mode, 3])
+                #     self.qList[3].put([mode, 3])
+                #     self.qList[4].put([mode, 3])
+                # elif data == "swipe_right":
+                #     self.qList[0].put([mode, 4])
+                #     self.qList[1].put([mode, 4])
+                #     self.qList[2].put([mode, 4])
+                #     self.qList[3].put([mode, 4])
+                #     self.qList[4].put([mode, 4])
                 elif data == "twirl":
                     self.qList[0].put([mode, 5])
                     self.qList[1].put([mode, 5])
                     self.qList[2].put([mode, 5])
                     self.qList[3].put([mode, 5])
                     self.qList[4].put([mode, 5])
-
+                elif data == "up":
+                    self.qList[5].put([mode, "up"])
+                elif data == "down":
+                    self.qList[5].put([mode, "down"])
             elif address == "/live":
                 mode = "live"
                 self.qList[0].put([mode, data])
